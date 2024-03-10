a6a56653f4224e80911a7d130bc61aa16a248752
alfre <tu@email.com>
2024-03-10 22:53:08 +0000

    primer codigo local

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('¡Hola mundo!');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
