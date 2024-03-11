3b217fea6c8c0c5ffcf5b4dd4c9d04dfafcfc1e7
alfre <tu@email.com>
2024-03-11 04:58:08 +0000

    ahora mal

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('¡Hola mundo a todos ahora FUNCIONA!');
+  res.send('¡Hola mundo a todos ahora FUNCIONA mal!');
 });
 
 const PORT = process.env.PORT || 3000;
