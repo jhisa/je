e6b112fb588a59086928f0f3bcfbe910d819cb57
alfre <tu@email.com>
2024-03-11 03:13:15 +0000

    volvemos al principio

--- index.js
+++ index.js
@@ -2,28 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/suma', (req, res) => {
-  // Obtener los parámetros 'num1' y 'num2' de la URL
-  const { num1, num2 } = req.query;
-
-  // Convertir los parámetros a números
-  const numero1 = parseInt(num1);
-  const numero2 = parseInt(num2);
-
-  // Si no se recibieron dos números, enviar un error
-  if (isNaN(numero1) || isNaN(numero2)) {
-    res.status(400).send({ error: "Parámetros no válidos." });
-    return;
-  }
-
-  // Calcular la suma
-  const suma = numero1 + numero2;
-
-  // Enviar la respuesta con la suma
-  res.send({ resultado: suma });
+app.get('/', (req, res) => {
+  res.send('¡Hola mundo!');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
-
